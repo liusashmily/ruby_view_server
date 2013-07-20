0f74f9c3bd4d217beaff166b00dab71a1189cd32
liusashmily <liusashmily@gmail.com>
2013-07-20 15:37:48 -0500

    homework section 3 completed

--- erb_example.rb
+++ erb_example.rb
@@ -7,3 +7,18 @@ puts template.result(binding)
 y = "Lisa"
 template1 = ERB.new "The name of y is: <%= y %>"
 puts template1.result(binding)
+
+z =['Amy','Johnathan','Jess']
+template2 = ERB.new "The people in whitehall are: "
+puts template2.result(binding)
+z.each do |name|
+	puts name
+end
+
+stones = { 1 => "garnet", 2 => "topaz", 
+           3 => "opal", 4 => "amethyst"
+         }
+
+puts stones.fetch 1
+puts stones[2]
+puts stones.values_at 1, 2, 3
