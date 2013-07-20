a8225f5500ba8e94ab31a9075c02307514d4c9d0
liusashmily <liusashmily@gmail.com>
2013-07-20 12:37:34 -0500

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,7 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = "Lisa"
+template1 = ERB.new "The name of y is: <%= y %>"
+puts template1.result(binding)
